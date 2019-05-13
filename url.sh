8b593c21d2e68de084c895782b0bdd16629cc7d4
Alexander Scott <xander@cadcoder.com>
2019-05-13 10:47:22 +1000

    Deployment

--- url.sh
+++ url.sh
@@ -13,10 +13,9 @@ shadow-cljs () {
 }
 
 ## stop:
-## Stops shadow-cljs and karma
+## Stops shadow-cljs
 stop () {
 	shadow-cljs stop &>/dev/null
-	pkill -f 'karma ' &>/dev/null
 }
 
 ## clean:
@@ -58,6 +57,59 @@ test-cljs () {
 	shadow-cljs compile node \
 	&& node target/node/test.js
 	abort_on_error 'node tests failed'
+}
+
+is-snapshot () {
+	version=$(cat VERSION)
+	[[ "$version" == *SNAPSHOT ]]
+}
+
+deploy () {
+	if [[ -n "$CIRCLECI" ]];then
+		lein with-profile install deploy clojars &>/dev/null
+		abort_on_error
+	else
+		lein with-profile install deploy clojars
+		abort_on_error
+	fi
+}
+
+## snapshot:
+## args: [-l]
+## Pushes a snapshot to Clojars
+## [-l] local
+snapshot () {
+	if is-snapshot;then
+		echo_message 'SNAPSHOT suffix already defined... Aborting'
+		exit 1
+	else
+		version=$(cat VERSION)
+		snapshot="$version-SNAPSHOT"
+		echo ${snapshot} > VERSION
+		echo_message "Snapshotting $snapshot"
+		case $1 in
+			-l)
+				lein with-profile install install
+				abort_on_error;;
+			*)
+				deploy;;
+		esac
+		echo "$version" > VERSION
+	fi
+}
+
+## release:
+## Pushes a release to Clojars
+release () {
+	version=$(cat VERSION)
+	if ! is-snapshot;then
+		version=$(cat VERSION)
+		echo_message "Releasing $version"
+		deploy
+	else
+		echo_message 'SNAPSHOT suffix already defined... Aborting'
+		exit 1
+	fi
 }
 
 script-invoke $@
