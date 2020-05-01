b401036182c3c7c238864b0cd2fa187086363703
Alexander Scott <xander@axrs.io>
2020-05-01 12:12:31 +1000

    Update package deps

--- project.clj
+++ project.clj
@@ -5,13 +5,12 @@
   :url "http://github.com/jesims/url"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :min-lein-version "2.9.0"
   :plugins [[lein-parent "0.3.8"]]
   :clean-targets ^{:protect false} ["target"]
-  :parent-project {:coords  [io.jesi/parent "3.9.0"]
-                   :inherit [:plugins :managed-dependencies :deploy-repositories :dependencies :profiles :aliases]}
+  :parent-project {:coords  [io.jesi/parent "3.10.0"]
+                   :inherit [:plugins :managed-dependencies :deploy-repositories :dependencies :profiles :aliases :min-lein-version]}
   :dependencies [[org.clojure/clojure :scope "provided"]
                  [org.clojure/clojurescript :scope "provided"]
                  [pathetic "0.5.1"]]
-  :profiles {:dev [:parent/dev {:dependencies [[io.jesi/customs "1.1.1"]
-                                               [thheller/shadow-cljs :scope "provided"]]}]})
+  :profiles {:dev [:parent/dev {:dependencies [[io.jesi/customs]
+                                               [thheller/shadow-cljs]]}]})
